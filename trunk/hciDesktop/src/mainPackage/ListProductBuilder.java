9cc97092113956c9b976d4b63758d7f1efd983b9
bombax <bombax@77f57708-cd88-11de-8d2e-8bf2cb4fc7ae>
2009-11-21 21:13:31 +0000

    SearchHandler andando

--- trunk/hciDesktop/src/mainPackage/ListProductBuilder.java
+++ trunk/hciDesktop/src/mainPackage/ListProductBuilder.java
@@ -15,9 +15,11 @@ public class ListProductBuilder extends JPanel{
 		GridLayout layout = new GridLayout(0,1);
 		this.setLayout(layout);
 		for(i=0;i<list.size();i++){
+			System.out.println("ACA");
 			ProductBuilder producto = new ProductBuilder(list.get(i));
 			//producto.setSize(10,5);
 			add(producto);
 		}
+		show();
 	}
 }
